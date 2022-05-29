d5d5c0b630b89cb0d06d552a1e9908101d9f583c
Victor V <victor.vergeau@polymtl.ca>
2022-05-28 21:33:14 -0400

    ajout de la table material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { TableViewerComponent } from './components/table-viewer/table-viewer.component';
+import { MatTableModule } from '@angular/material/table'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableViewerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
