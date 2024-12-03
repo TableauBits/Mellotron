232e2571153e96866cbc86e5f3c48448d9f71f11
Hugo Vandenbroucke-Menu <hugo.vdbm@icloud.com>
2024-12-03 18:55:53 -0500

    Add expanded rows

--- src/app/components/table-viewer/table-viewer.component.ts
+++ src/app/components/table-viewer/table-viewer.component.ts
@@ -11,16 +11,26 @@ import { DataManagerService, DataSong } from 'src/app/services/data-manager.serv
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { MatDialog } from '@angular/material/dialog';
 import { HelpWindowComponent } from '../help-window/help-window.component';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-table-viewer',
   templateUrl: './table-viewer.component.html',
-  styleUrls: ['./table-viewer.component.scss']
+  styleUrls: ['./table-viewer.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class TableViewerComponent implements AfterViewInit {
 
   displayedColumns: string[];
   dataSource: MatTableDataSource<DataSong>;
+
+  expandedElement?: DataSong;
 
   selectedColumns: FormControl;
   selectedColumnsList: string[];
