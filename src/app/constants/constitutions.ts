287b71b209222b5836b32ae424b2e448d0de337a
hvdbm <hugo.vdbm@icloud.com>
2026-01-23 10:58:04 -0500

    Ajout des données de la constitution X16 (#77)

--- src/app/constants/constitutions.ts
+++ src/app/constants/constitutions.ts
@@ -34,6 +34,7 @@ import X12 from '../../assets/constitutions/X12.json';
 import X13 from '../../assets/constitutions/X13.json';
 import X14 from '../../assets/constitutions/X14.json';
 import X15 from '../../assets/constitutions/X15.json';
+import X16 from '../../assets/constitutions/X16.json';
 
 export type DataConstitution = {
   cstName: string;
@@ -51,6 +52,6 @@ export type DataConstitution = {
 export const ALL_CONSTITUTIONS_DATA: DataConstitution[] = [
   C1, C2, C3, C4, C5, C6, C7, C8, C9, C10,
   T1, T2, T3,
-  X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15,
+  X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, X11, X12, X13, X14, X15, X16,
   XM, XM2
 ]
